b3039bfe90b3a2c8e0b90f5f0d7b2db2b3b12722
Zac Lo <zachary.lo200@gmail.com>
2024-11-06 14:18:34 +1000

    fix part 2

--- IBudget.GUI/Views/TagsPageView.axaml.cs
+++ IBudget.GUI/Views/TagsPageView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using IBudget.GUI.ViewModels;
 
 namespace IBudget.GUI.Views;
 
@@ -7,5 +9,14 @@ public partial class TagsPageView : UserControl
     public TagsPageView()
     {
         InitializeComponent();
+        Loaded += TagsPage_Loaded;
+    }
+
+    private void TagsPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is TagsPageViewModel viewModel)
+        {
+            viewModel.RefreshView();
+        }
     }
 }
