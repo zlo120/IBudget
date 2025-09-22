79b35276736520ad206f27e299358ec84c0ef63b
Zac Lo <zachary.lo@gmail.com>
2025-09-22 17:42:21 +1000

    fixes

---
+++ IBudget.GUI/Views/DataView/DataTableView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace IBudget.GUI;
+
+public partial class DataTableView : UserControl
+{
+    public DataTableView()
+    {
+        InitializeComponent();
+    }
+}
